a681ca6cc892327faebf0d16eed4ad5bbb230e97
Keith Austin <datketh@gmail.com>
2023-09-12 01:30:19 -0400

    Update test

--- testing.nim
+++ testing.nim
@@ -1,40 +1,68 @@
 import ./src/[WorldTree, Component, Systems]
 import terminal
+import times
+import std/monotimes
 
 # Test component, just a flag
 type
-  Existence = ref object of Component
   Age = ref object of Component
-    ticks = 0
+    seconds = 0.0
+
+  Clock = ref object of Component
+    lastTickTime: MonoTime
+    currTickTime: MonoTime
+    deltaTime: float
 
   Time = ref object of System
+  ClockSystem = ref object of System
 
-proc onTick(self: Time, tree: var WorldTree) =
+# Increases affected entities' ages in seconds
+proc onTick(self: Time, tree: var WorldTree, deltaTime: float64) =
  for entity in self.entities:
-   tree.getComponent[:Age](entity).ticks += 1
+  tree.getComponent[:Age](entity).seconds += (1 * deltaTime)
+
+# Calculates the time between each tick to keep a consistent clock
+proc onTick(self: ClockSystem, tree: var WorldTree) =
+  for entity in self.entities:
+    let currTime = getMonoTime()
+    let lastTime = tree.getComponent[:Clock](entity).lastTickTime
+    let microseconds = (currTime - lastTime).inMicroseconds
+
+    tree.getComponent[:Clock](entity).deltaTime = microseconds / 1_000_000
+    tree.getComponent[:Clock](entity).lastTickTime = currTime
 
 # Instantiate a tree and register the Existence component
 var tree = WorldTree()
-tree.registerComponentType[:Existence]()
 tree.registerComponentType[:Age]()
+tree.registerComponentType[:Clock]()
 
-# Register the Time system
+# Register the Time and Clock systems
 var time = tree.registerSystem[:Time]()
+var clockSystem = tree.registerSystem[:ClockSystem]()
 
+# Set the Time system's signature
 var signature: set[ComponentType]
 signature.incl(tree.getComponentType[:Age]())
 tree.setSystemSignature[:Time](signature)
 
-# Create a new entity and put an Existence flag on it
+# Set the Clock system's signature
+signature.excl(tree.getComponentType[:Age])
+signature.incl(tree.getComponentType[:Clock])
+tree.setSystemSignature[:ClockSystem](signature)
+
+# Create a new entity and put an Age component on it
 var e1 = tree.newEntity()
-tree.addComponent[:Existence](e1, Existence())
 tree.addComponent[:Age](e1, Age())
 
-var age = 0.0
+# Create a new entity to carry the Clock component
+var clock = tree.newEntity()
+tree.addComponent[:Clock](clock, Clock(lastTickTime: getMonoTime()))
 
-while age < 100.0:
-  time.onTick(tree)
-  echo " "
-  age = (tree.getComponent[:Age](e1).ticks / 10_000)
+# Continously run the Clock and Time systems until e1 is 10 seconds old, 
+# printing its age each frame
+while tree.getComponent[:Age](e1).seconds < 10.0:
+  clockSystem.onTick(tree)
+  time.onTick(tree, tree.getComponent[:Clock](clock).deltaTime)
   eraseScreen()
-  echo age
+  let age = tree.getComponent[:Age](e1).seconds.toInt()
+  echo "Entity is ", age, " seconds old"
